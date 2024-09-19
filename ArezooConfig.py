c00344d30aaf0787825642751085bdd23972644c
Arezoo Amani <64693382+arezoo17@users.noreply.github.com>
2024-09-19 23:33:30 +0200

    Add files via upload

---
+++ ArezooConfig.py
@@ -0,0 +1,10 @@
+#create the function of config
+
+def load_config():
+    return {
+        "host": "localhost",
+        "database": "postgres",
+        "user" : "postgres",
+        "password": "****",
+        "port": "5433"
+    }
